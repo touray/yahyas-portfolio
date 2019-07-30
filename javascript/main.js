f1feddb60e1c1ade86744e47fd3bcb79665b727d
yahya <sankarrynjieb@gmai.com>
2019-07-30 19:41:14 +0200

    html-css-js

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+var x = document.createElement("BODY");
+function myFunction() {
+  var x = document.getElementsByClassName("yahyas-portfolio");
+  document.getElementById("demo").innerHTML;
+}
+myFunction();
+console.log("start");
+
+
