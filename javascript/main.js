424ff94c138390df801d69b811bb01dcaee7cef2
yahya <sankarrynjieb@gmai.com>
2019-08-01 19:40:42 +0200

    link tag

--- javascript/main.js
+++
@@ -1,9 +0,0 @@
-var x = document.createElement("BODY");
-function myFunction() {
-  var x = document.getElementsByClassName("yahyas-portfolio");
-  document.getElementById("demo").innerHTML;
-}
-myFunction();
-console.log("start");
-
-
